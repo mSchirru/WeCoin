3a5e22fc0fc2795a5273d53be6e3ae42987d75ef
Joao Pedro <jpfaria19@hotmail.com>
2018-03-30 19:19:35 -0300

    Testando autenticacao da WebAPI no projeto MVC

--- Presentation/Controllers/HomeController.cs
+++ Presentation/Controllers/HomeController.cs
@@ -1,30 +1,100 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using Newtonsoft.Json.Linq;
 
 namespace Presentation.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+
+        const string userName = "noisefoda@foda.com.br";
+        const string password = "$oTemFoda";
+        const string apiBaseUri = "http://localhost:2178/";
+        const string apiGetValues = "/api/values";
+
+        static void Main(string[] args)
         {
-            return View();
+            //Get the token
+            var token = GetAPIToken(userName, password, apiBaseUri).Result;
+            Console.WriteLine("Token: {0}", token);
+
+            //Make the call
+
+            var response = GetRequest(token, apiBaseUri, apiGetValues).Result;
+            Console.WriteLine("response: {0}", response);
+
+            //wait for key press to exit
+            Console.ReadKey();
         }
 
-        public ActionResult About()
+        private static async Task<string> GetAPIToken(string userName, string password, string apiBaseUri)
         {
-            ViewBag.Message = "Your application description page.";
+            using (var client = new HttpClient())
+            {
+                //setup client
+                client.BaseAddress = new Uri(apiBaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
-            return View();
+
+                //setup login data
+                var formContent = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("grant_type", "password"),
+                    new KeyValuePair<string, string>("username", userName),
+                    new KeyValuePair<string, string>("password", password),
+                });
+
+                //send request
+                HttpResponseMessage responseMessage = await client.PostAsync("/Token", formContent);
+
+                //get access token from response body
+                var responseJson = await responseMessage.Content.ReadAsStringAsync();
+                var jObject = JObject.Parse(responseJson);
+                return jObject.GetValue("access_token").ToString();
+            }
         }
 
-        public ActionResult Contact()
+        static async Task<string> GetRequest(string token, string apiBaseUri, string requestPath)
         {
-            ViewBag.Message = "Your contact page.";
+            using (var client = new HttpClient())
+            {
+                //setup client
+                client.BaseAddress = new Uri(apiBaseUri);
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("Authorization", "Bearer " + token);
 
-            return View();
+                //make request
+                HttpResponseMessage response = await client.GetAsync(requestPath);
+                var responseString = await response.Content.ReadAsStringAsync();
+                return responseString;
+            }
         }
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
     }
 }
