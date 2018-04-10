5466aa9863dab61863eabe5577686d45be8cf697
Diego Victor Jesus <mcmrcatra@outlook.com>
2018-04-09 21:40:04 -0300

    Acertando commit

--- Presentation/ViewModels/FriendshipViewModel.cs
+++ Presentation/ViewModels/FriendshipViewModel.cs
@@ -1,6 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using Domain.Entities;
+﻿using Domain.Entities;
 
 namespace Presentation.ViewModels
 {
