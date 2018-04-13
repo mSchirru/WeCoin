b541f0e741ff4723731c9f0e272d5de5a14d3264
Diego Victor Jesus <mcmrcatra@outlook.com>
2018-04-13 05:42:05 -0300

    Implementado logout (graças a deus); Implementada edição de informações pessoais do usuário; Implementado exibição de imagens de perfil dos usuários utilizando BLOB do Azure; Remoção das entidades de carteira e moeda;

--- Presentation/ViewModels/ApplicationUserViewModel.cs
+++ Presentation/ViewModels/ApplicationUserViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Domain.Entities;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Presentation.ViewModels
 {
@@ -15,9 +16,11 @@ namespace Presentation.ViewModels
         public bool TwoFactorEnabled { get; set; }
         public string PhoneNumber { get; set; }
         public string PhoneNumberConfirmed { get; set; }
+        [Required]
         public DateTime BirthDate { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
+        public string WalletAddress { get; set; }
 
         //public virtual ICollection<FriendshipViewModel> Friendships { get; set; }
         public ICollection<FriendshipViewModel> Friends { get; set; }
