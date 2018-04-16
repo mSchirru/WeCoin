175ce618d6497cbee2d8354bdaf4607ecd5a9629
Diego Victor Jesus <mcmrcatra@outlook.com>
2018-04-16 04:18:58 -0300

    Implementação da remoção de publicações de usuário;

---
+++ Repositories/Repositories/PostRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Domain.Interfaces.Repositories;
+using System.Linq;
+
+namespace Repositories.Repositories
+{
+    public class PostRepository : BaseRepository<Post>, IPostRepository
+    {
+        public int RemovePost(int postId)
+        {
+            Post post = Rc.Posts.SingleOrDefault(n => n.PostId == postId);
+            return Remove(post);
+        }
+    }
+}
