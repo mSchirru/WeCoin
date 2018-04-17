9e0a6c8f303e444518ab8b18a8188dfaa37c7cc6
Diego Victor Jesus <mcmrcatra@outlook.com>
2018-04-17 01:40:08 -0300

    Adição de captcha ao login;Acerto no cálculo de idade;Tratamento no tamanho de publicações muito grandes

---
+++ Presentation/ViewModels/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Presentation.ViewModels
+{
+    public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Psw { get; set; }
+    }
+}
